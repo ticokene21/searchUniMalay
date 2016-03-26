0f39fc86cf39085f391e3476f0051fba25d63eb2
vi luong <vi luong>
2016-03-26 08:49:23 +0700

    add code 1

--- myphp/Search.php
+++ myphp/Search.php
@@ -3,13 +3,17 @@ require_once('DB_driver.php');
 $conn = new DB_driver();
 $conn->open_connect();
 if(isset($_POST['data'])){
-	echo "co";
     $arr_val = array();
+    // echo '-------------';
+    // print_r(json_decode($_POST['data']));
+    // echo '-------------';
     $arr_val = json_decode($_POST['data']);//Bắt dữ liệu json bên ajax vào chuyển thàng kiểu array()
     $arr_course = array();
     $arr_uni = array();
-    echo count($arr_val);
-    echo $arr_val[1]->req_id;
+    $arr_result = array();
+    // var_dump($_POST['data']);
+    // echo count($arr_val);
+    
 
 //	echo count($arr_val);
   //   $sql="select * from cou_req";
@@ -19,33 +23,45 @@ if(isset($_POST['data'])){
   //   }
   //   else{
   //       $row=mysql_fetch_array($query)
-        for($i = 0;$i<count($arr_val)-1;$i++)
+        for($i = 0;$i<count($arr_val);$i++)
 		{
-		    $sql="select DISTINCT course_id from cou_req where req_id = '$arr_val[$i]->req_id' and valueother= '$arr_val[$i]->grade'";
-		    $query=mysql_query($sql);
+			if($arr_val[$i]->req_id == 21)
+			{
+				$sql="select DISTINCT course_id from cou_req where req_id = {$arr_val[$i]->req_id} and valuea <= {$arr_val[$i]->grade_a} and valueb <= {$arr_val[$i]->grade_b} and valuec <= {$arr_val[$i]->grade_c} ";
+			    $query=mysql_query($sql);
+			}else{
+			    $sql="select DISTINCT course_id from cou_req where req_id = {$arr_val[$i]->req_id} and valueorther <= {$arr_val[$i]->grade}";
+			    $query=mysql_query($sql); //var_dump($query );`
+			}
+		    if ($query)
 		    while ($row = mysql_fetch_array($query,MYSQL_ASSOC)) {
-		    	echo $row['course_id'];
+		    	// echo '-------------';
 		     	array_push($arr_course, $row['course_id']);
 		    }
-		}
-
+		}	
+		
 		$arr_course = array_unique($arr_course);
+		// var_dump($arr_course);
 //		echo $arr_course;
         for ($i=0; $i < count($arr_course) ; $i++) {
-       		$sql1= "select uni_id from course where course_id="+$arr_course[$i];
+       		$sql1= "select uni_id from course where course_id={$arr_course[$i]}";
+       		// echo $arr_course[$i];
        		$query1 = mysql_query($sql1);
-       		while ($row1 = mysql_fetch_array($query1)) {
+       		while ($row1 = mysql_fetch_array($query1,MYSQL_ASSOC)) {
        			array_push($arr_uni,$row1['uni_id']);
        		}
        	}
        	$arr_uni = array_unique($arr_uni);
         for ($i=0; $i < count($arr_uni) ; $i++) {
-        	$sql2 = "select * from uni where uni_id ="+$arr_uni[$i];
+        	$sql2 = "select * from uni where uni_id ={$arr_uni[$i]}";
         	$query2 = mysql_query($sql2);
-        	while ($row2 = mysql_fetch_array($query2)) {
+        	while ($row2 = mysql_fetch_array($query2,MYSQL_ASSOC)) {
         		echo $row2['name_uni'];
+        		echo '</br>';
         	}
         }
+        
+        
 
 }else
 	echo"khong";
